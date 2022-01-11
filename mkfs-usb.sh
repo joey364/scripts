ef80be577f0523a4f3dd65912aa29cb2734ba9dd
joey364 <71462893+joey364@users.noreply.github.com>
2022-01-11 10:26:42 +0000

    mkfs-usb: prettify output a bit

--- mkfs-usb.sh
+++ mkfs-usb.sh
@@ -2,9 +2,10 @@
 
 # find the drive
 echo "Filesystem      Mounted on"
-df | awk -v OFS='\t' '/\/dev\// {print $1, $6}'
+df | awk -v OFS='\t' '/\/dev\/s/ {print $1, $6}'
 
 # read mount point of usb
+echo ""
 echo "Enter the mount point of your usb drive.. e.g. /dev/sdb1"
 read mpoint
 
@@ -19,6 +20,7 @@ else
 fi
 
 # make the new filesystem on the drive
+echo ""
 echo "Enter the filesystem with which you'd like to format the usb with.."
 echo "Options: "
 echo "fat:    all operating systems"
@@ -26,6 +28,7 @@ echo "exfat:  all operating systems "
 echo "ntfs:   windows, linux*, macos* "
 echo "ext4:   linux, windows*"
 echo "btrfs:  linux only"
+echo ""
 
 fs_types=("fat" "exfat" "ntfs" "ext4" "btrfs")
 read fs_option
