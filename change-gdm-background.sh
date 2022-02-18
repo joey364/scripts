b60a9986882a1fed5282f4f64368e4ac6718358f
joey364 <71462893+joey364@users.noreply.github.com>
2022-02-18 01:58:44 +0000

    outline change-gdm-background

---
+++ change-gdm-background.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# extract current shell theme to working directory
+
+# create a shell theme gresource files
+
+# copty prefered image to working directory
+
+# edit gresource file to include new image
+
+# edit gnome-shell-theme.css
+
+# change background from solid color to image
+
+# compile the new gnome shell gresource file
+
+# copy output binary to /usr/share/gnome-shell/
+
+# restart gdm.servie for changes to take effect
