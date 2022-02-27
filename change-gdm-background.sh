8184304f6d7acc1d886c73d44be0af4490d8b199
joey364 <71462893+joey364@users.noreply.github.com>
2022-02-27 15:07:47 +0000

    gdm-background: add gresource dependency check

--- change-gdm-background.sh
+++ change-gdm-background.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# pre-check for gresource package
+# Debian
+# apt-get install libglib2.0-0-dbg
+# Arch Linux
+# pacman -S glib2
+# Fedora
+# dnf install glib2-devel
+
+tmp_dir=$(mktemp -d)
+
+# check dependencies
+check_dependencies() {
+	if [ "$(cammand -v gresource)" ]; then
+		echo "gresource installed.."
+	elif [ -f "/etc/arch-release" ]; then
+		echo "instaliing gresource with pacman.."
+		pacman -S glib2
+	elif [ -f "/etc/fedora-release" ]; then
+		echo "instaliing gresource with dnf.."
+		dnf install glib2-devel
+	else
+		echo "instaliing gresource with apt.."
+		apt-get install libglib2.0-bin
+	fi
+}
+
 # extract current shell theme to working directory
 
 # create a shell theme gresource files
