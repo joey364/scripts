f3ed1aa13ba39973af6e55bfdfa368edf1880163
joey364 <71462893+joey364@users.noreply.github.com>
2022-03-01 11:26:45 +0000

    gdm: add extractgst function

--- change-gdm-background.sh
+++ change-gdm-background.sh
@@ -27,6 +27,24 @@ check_dependencies() {
 }
 
 # extract current shell theme to working directory
+extract_gdm_theme() {
+	echo "Extracting gst theme"
+	gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
+	# workdir=${HOME}/shell-theme
+	workdir=$tmp_dir
+
+	for r in $(gresource list $gst); do
+		r=${r#\/org\/gnome\/shell/}
+		if [ ! -d $workdir/${r%/*} ]; then
+			mkdir -p $workdir/${r%/*}
+		fi
+	done
+
+	for r in $(gresource list $gst); do
+		gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
+	done
+
+}
 
 # create a shell theme gresource files
 
