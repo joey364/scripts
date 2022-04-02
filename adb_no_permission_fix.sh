dc7354564672749614d32089b3bad567e80afff9
joey364 <71462893+joey364@users.noreply.github.com>
2022-04-02 12:58:19 +0000

    adb_fix: refactor

--- adb_no_permission_fix.sh
+++ adb_no_permission_fix.sh
@@ -9,8 +9,12 @@ sudo chown root:joel $adb_location
 
 sudo chmod 4550 $adb_location
 
+echo "Killing adb server.."
 sudo adb kill-server
 
+echo "Starting adb server.."
 sudo adb start-server
 
 # TODO: confirm permissions restored
+
+adb devices
