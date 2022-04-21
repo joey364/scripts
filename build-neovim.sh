20f8414f9eb120f47172f66f2834d6e0be215d27
joey364 <71462893+joey364@users.noreply.github.com>
2022-04-21 17:30:23 +0000

    build-nvim: refactor

--- build-neovim.sh
+++ build-neovim.sh
@@ -10,11 +10,25 @@
 # Arch Linux
 # sudo pacman -S base-devel cmake unzip ninja tree-sitter curl
 
+# install type options
+# build from remote
+# build from local repo
+# build stable, release, @version
 # take input for install type
-install_type=$1
+# install_type=$1
+#; TODO: ADD SUPPORT FOR CUSTOM PATH FOR LOCAL REPO
 
+_print_header() {
+	cat <<'EOF'
+ _           _ _     _              _           
+| |__  _   _(_) | __| |  _ ____   _(_)_ __ ___  
+| '_ \| | | | | |/ _` | | '_ \ \ / / | '_ ` _ \ 
+| |_) | |_| | | | (_| | | | | \ V /| | | | | | |
+|_.__/ \__,_|_|_|\__,_| |_| |_|\_/ |_|_| |_| |_|
+                                                
+EOF
+}
 # install build dependencies
-install_build_deps() {
 	echo "installing build dependencies.."
 	os_type=$(uname -s)
 	case "$os_type" in
@@ -25,10 +39,10 @@ install_build_deps() {
 				pkgconfig unzip patch gettext curl
 		elif [[ -f "/etc/arch-release" ]]; then
 			echo "i use arch btw.."
-			sudo pacman -S base-devel cmake unzip ninja tree-sitter curl
+			sudo pacman -Sy base-devel cmake unzip ninja tree-sitter curl
 		else
 			echo "installing build dependencies with apt-get"
-			sudo apt-get install ninja-build \
+			sudo apt-get -y install ninja-build \
 				gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 		fi
 		;;
@@ -40,85 +54,24 @@ install_build_deps() {
 }
 
 # make command to build release version of nvim
-make_release() {
 	sudo make install CMAKE_BUILD_TYPE=Release
 }
 
 # make command to build nightly version of nvim
-make_nightly() {
 	sudo make install CMAKE_BUILD_TYPE=Debug
 }
 
-# clone neovim repo and build
-install_with_clone() {
-	if [[ $1 == 'stable' ]]; then
-		echo "Installing stable release of neovim"
-		git clone --branch stable --single-branch https://github.com/neovim/neovim
-		cd neovim && make_release
-	elif [[ $! == 'dev' ]]; then
-		echo "Installing nightly release of neovim"
-		git clone https://github.com/neovim/neovim --depth=1
-		cd neovim && make_nightly
-	else
-		echo 'Pass stable or dev to script and try again..'
-		echo 'e.g. sudo ./build-neovim.sh dev'
-		exit 0
-	fi
 }
 
-# use already existing repo
-install_without_clone() {
-	sudo cp -r "$HOME/neovim" .
-	cd $HOME/neovim && git pull
-	if [[ $1 == 'stable' ]]; then
-		sudo git checkout stable && make_release
-	elif [[ $1 == 'dev' ]]; then
-		sudo git checkout master && make_nightly
-	else
-		echo 'Pass stable or dev to script and try again..'
-		echo 'e.g. sudo ./build-neovim.sh dev'
-		exit 0
-	fi
 }
 
-cleanup() {
-	cd .. && sudo rm -rf neovim/
+
 }
 
-print_header() {
-	cat <<'EOF'
- _           _ _     _              _           
-| |__  _   _(_) | __| |  _ ____   _(_)_ __ ___  
-| '_ \| | | | | |/ _` | | '_ \ \ / / | '_ ` _ \ 
-| |_) | |_| | | | (_| | | | | \ V /| | | | | | |
-|_.__/ \__,_|_|_|\__,_| |_| |_|\_/ |_|_| |_| |_|
-                                                
-EOF
-}
 
-main() {
-
-	print_header
-
-	# require sudo to run script
-	if [[ $EUID -ne 0 ]]; then
-		echo "This script must be run as root, use sudo $0 instead" 1>&2
-		exit 1
 	fi
 
-	# create a temp directory and move into it
-	cd "$(mktemp -d)" || return
 
-	# install_build_deps
 
-	# check for nvim repo locally
-	if [[ -d "$HOME/neovim" ]]; then
-		echo "nvim repo already here"
-		install_without_clone $install_type
-	else
-		echo "nvim repo not here"
-		install_with_clone $install_type
-	fi
 }
 
-main
