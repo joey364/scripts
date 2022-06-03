d92c5079bc8b98f122c3b2ec3cedb4fe160f3cc5
joey364 <71462893+joey364@users.noreply.github.com>
2022-06-03 00:37:36 +0000

    extractgst: refactor

--- extractgst.sh
+++ extractgst.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
-workdir=${HOME}/shell-theme
+workdir=${HOME}/shell-theme2
 
 for r in $(gresource list $gst); do
 	r=${r#\/org\/gnome\/shell/}
-	if [ ! -d $workdir/${r%/*} ]; then
-		mkdir -p $workdir/${r%/*}
+	if [ ! -d "$workdir"/"${r%/*}" ]; then
+		mkdir -p "$workdir"/"${r%/*}"
 	fi
 done
 
 for r in $(gresource list $gst); do
-	gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
+	gresource extract $gst "$r" >"$workdir"/"${r#\/org\/gnome\/shell/}"
 done
