30e9e68ef1ec7a3de4cc8fbe52114e77587d5f9f
joey364 <71462893+joey364@users.noreply.github.com>
2022-09-01 09:45:05 +0000

    sync_datetime: use random spinners

--- sync_datetime.sh
+++ sync_datetime.sh
@@ -2,10 +2,15 @@
 
 is_connected=$(nmcli general status | grep '^connected' >/dev/null && echo "yay")
 
+spinner_options=("dot" "line" "minidot" "jump" "pulse" "points" "globe" "moon" "monkey" "meter" "hamburger")
+
+# select random index from from the spinner_options array
+rand_idx=$(shuf -i 0-$((${#spinner_options[@]} - 1)) -n 1)
+
 if [[ $is_connected = 'yay' ]]; then
 	echo connected
-	# gum spin --spinner="meter" --title="syncing datetime" -- sudo htpdate -s google.com
-	sudo htpdate -s google.com
+	gum spin --spinner="${spinner_options[rand_idx]}" --show-output --title="syncing datetime" -- sudo htpdate -s google.com
+	# sudo htpdate -s google.com
 else
 	echo not connected to the internet
 	echo please get connected
